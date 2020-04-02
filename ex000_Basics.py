a5ce7d126b71544a8ea3601092eabfa884daabf3
arcticv <62716600+arcticv@users.noreply.github.com>
2020-04-02 13:53:56 -0400

    Update ex000_Basics.py

--- ex000_Basics.py
+++ ex000_Basics.py
@@ -521,6 +521,27 @@ def count_primes(num):
 	print(primes)
 	return len(primes)
 	
+
+# pandas melt
+df = pd.DataFrame({'A': {0: 'a', 1: 'b', 2: 'c'},
+                   'B': {0: 1, 1: 3, 2: 5},
+                   'C': {0: 2, 1: 4, 2: 6}})
+"""		
+df
+   A  B  C
+0  a  1  2
+1  b  3  4
+2  c  5  6
+"""
+pd.melt(df, id_vars=['A'], value_vars=['B'])
+
+"""
+   A variable  value
+0  a        B      1
+1  b        B      3
+2  c        B      5
+"""
+
 # hi
 
 
